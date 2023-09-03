6cf36b603c1ef3f80db1eace385fa92732ed93a4
Ryan Yogan <ryanyogan@gmail.com>
2023-09-03 00:46:03 -0500

    feat // mobile and a header

--- src/components/server/server-channel.tsx
+++ src/components/server/server-channel.tsx
@@ -33,7 +33,9 @@ export const ServerChannel = ({
 
   return (
     <button
-      onClick={() => {}}
+      onClick={() => {
+        router.push(`/servers/${params?.serverId}/channels/${channel.id}`);
+      }}
       className={cn(
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
         params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
